5eaf9b3bcd6325a699fe7b47e79eeb74465f2d10
Daniel Egele <danielegele9@gmail.com>
2023-12-06 16:27:03 +0100

    all added files

---
+++ js/Todo.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+   const elementTotoggle = document.getElementById("elementTotoggle");
+   const toggleButton = document.getElementById("toggleButton");
+
+   toggleButton.addEventListener("click",()=>{
+    if(elementTotoggle.style.textDecoration=="none"){
+        elementTotoggle.style.textDecoration = "line-through";
+        toggleButton.classList.toggle("mybtn");
+    }else{
+        elementTotoggle.style.textDecoration = "none";
+    }
+
+   });
+});
