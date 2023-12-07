5107f0f5bbba2058a515894a00ac99ea97278dbe
Daniel Egele <danielegele9@gmail.com>
2023-12-07 13:38:24 +0100

    all added files

--- js/Todo.js
+++ js/Todo.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded",()=>{
    toggleButton.addEventListener("click",()=>{
     if(elementTotoggle.style.textDecoration=="none"){
         elementTotoggle.style.textDecoration = "line-through";
-        toggleButton.classList.toggle("mybtn");
+        // toggleButton.classList.toggle("mybtn");
     }else{
         elementTotoggle.style.textDecoration = "none";
     }
