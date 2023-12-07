193af504df5152102664292a61b2dd80e201ea72
Daniel Egele <danielegele9@gmail.com>
2023-12-07 14:00:00 +0100

    all added files

--- js/Todo.js
+++ js/Todo.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded",()=>{
-   const elementTotoggle = document.getElementById("elementTotoggle");
-   const toggleButton = document.getElementById("toggleButton");
+//    const elementTotoggle = document.getElementById("elementTotoggle");
+//    const toggleButton = document.getElementById("toggleButton");
 
-   toggleButton.addEventListener("click",()=>{
+//    toggleButton.addEventListener("click",()=>{
+//     if(elementTotoggle.style.textDecoration=="none"){
+//         elementTotoggle.style.textDecoration = "line-through";
+//         // toggleButton.classList.toggle("mybtn");
+//     }else{
+//         elementTotoggle.style.textDecoration = "none";
+//     }
+
+//    });
+});
+
+function myTodoList(){
+    const elementTotoggle = document.getElementById("elementTotoggle");
+    const toggleButton = document.getElementById("toggleButton");
     if(elementTotoggle.style.textDecoration=="none"){
         elementTotoggle.style.textDecoration = "line-through";
-        // toggleButton.classList.toggle("mybtn");
+        toggleButton.classList.add("mybtn");
     }else{
         elementTotoggle.style.textDecoration = "none";
     }
 
-   });
-});
+   
+}
